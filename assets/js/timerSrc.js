5bd347408571299b8f4f4808d05855966fdaa9cd
Alexander Kostadinov <80641156+AlexanderKK@users.noreply.github.com>
2023-01-28 21:53:28 +0200

    Update timerSrc.js

--- assets/js/timerSrc.js
+++ assets/js/timerSrc.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * HTML Element
+ * HTML Element Declaration
  */
 const timerHours = document.querySelector(".select--hours select");
 const timerMinutes = document.querySelector(".select--minutes select");
